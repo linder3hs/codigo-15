d162208dbf555440516dd869d774dac95095eba8
linder3hs <linderhassinger00@gmail.com>
2023-09-13 20:25:33 -0500

    forms

--- semana7/js/forms.js
+++ semana7/js/forms.js
@@ -1,9 +1,14 @@
 const form = document.querySelector("#form-register");
 
-/**
- * Los formularios tiene un evento especial y unico
- * onsubmit tiene un parametro llamando event
- */
+function verifyIfEmptyInput(element) {
+  if (element.value.length > 0) {
+    element.classList.remove("border", "border-red-500");
+  } else {
+    if (element.dataset.validation !== "false") {
+      element.classList.add("border", "border-red-500");
+    }
+  }
+}
 
 form.onsubmit = function (event) {
   // Existe una funcion que nos permite evitar que el navegador
@@ -11,20 +16,18 @@ form.onsubmit = function (event) {
   event.preventDefault();
 
   const inputs = document.querySelectorAll("input");
-
   const values = {};
 
   for (const input of inputs) {
     values[input.name] = input.value;
+    verifyIfEmptyInput(input)
   }
 
   // vamos a guardar los valores del objeto en un array y verificar si alguno esta vacio
   const inputValues = Object.values(values);
 
   const validacion = inputValues.find((value) => !value);
-  
-  console.log(typeof validacion);
-  
+
   if (typeof validacion === "string") {
     alert("Completo todos los campos");
     return;
