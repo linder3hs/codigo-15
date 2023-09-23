012288e656507e6dbb3e01d076a287601c1d8130
linder3hs <linderhassinger00@gmail.com>
2023-09-22 20:11:40 -0500

    ➕ feat: add movies from api

--- vanilla-project/main.js
+++ vanilla-project/main.js
@@ -1,5 +1,5 @@
 import "./style.css";
-import { getGenres } from "./movie";
+import { getGenres, getGenreByURL } from "./movie";
 
 document.querySelector("#app").innerHTML = `
   <div class="p-6 max-w-[60%] m-auto">
@@ -16,12 +16,8 @@ document.querySelector("#app").innerHTML = `
         </div>
       </div>
       <div>
-        <div class="grid grid-cols-3 gap-20">
-          <div>
-            <img class="w-[200px]" src="https://cdn.apis.cineplanet.com.pe/CDN/media/entity/get/FilmPosterGraphic/HO00001790?referenceScheme=HeadOffice&allowPlaceHolder=true" />
-            <h2 class="font-semibold text-xl mt-2">After: Para Siempre</h2>
-            <p>Drama, 1h 40min, +14.</p>
-          </div>
+        <div id="grid-movies" class="grid grid-cols-3 gap-20">
+         
         </div>
       </div>
     </div>
@@ -29,3 +25,4 @@ document.querySelector("#app").innerHTML = `
 `;
 
 getGenres(document.querySelector("#container-genres"));
+getGenreByURL()
--- vanilla-project/movie.js
+++ vanilla-project/movie.js
@@ -1,16 +1,56 @@
 const urlGenres = "https://moviesminidatabase.p.rapidapi.com/genres/";
+const urlMoviesByGenre =
+  "https://moviesminidatabase.p.rapidapi.com/movie/byGen/";
+
+const options = {
+  headers: {
+    "X-RapidAPI-Key": "4cc4c577cdmsh6e25517382b8781p16d967jsn992e2eeb5bdc",
+    "X-RapidAPI-Host": "moviesminidatabase.p.rapidapi.com",
+  },
+};
+
+function renderItemList(genre) {
+  return `
+    <p class="my-2 hover:text-blue-800 hover:font-semibold">
+      <a href="?genre=${genre}" class="cursor-pointer">${genre}</a>
+    </p>
+  `;
+}
 
 export async function getGenres(element) {
-  const response = await fetch(urlGenres, {
-    headers: {
-      "X-RapidAPI-Key": "4cc4c577cdmsh6e25517382b8781p16d967jsn992e2eeb5bdc",
-      "X-RapidAPI-Host": "moviesminidatabase.p.rapidapi.com",
-    },
-  });
-
+  const response = await fetch(urlGenres, options);
   const data = await response.json();
 
   data.results.forEach((item) => {
-    element.innerHTML += `<p class="my-2 hover:text-blue-800 hover:font-semibold"><a class="cursor-pointer">${item.genre}</a></p>`;
+    element.innerHTML += renderItemList(item.genre);
+  });
+}
+
+function renderMovie(movie) {
+  return `
+    <div>
+      <img class="w-[200px]" src="" />
+      <h2 class="font-semibold text-xl mt-2">${movie.title}</h2>
+      <p></p>
+    </div>
+  `;
+}
+
+export async function getGenreByURL() {
+  const genre = location.search.split("=")[1];
+
+  // caso1: si hay un error
+  if (!genre) return;
+
+  // caso2: cuando no hay error (hacemos la busqueda)
+  // https://moviesminidatabase.p.rapidapi.com/movie/byGen/genre
+  const response = await fetch(`${urlMoviesByGenre}${genre}`, options);
+  const data = await response.json();
+
+  const containerMovies = document.querySelector("#grid-movies");
+
+  data.results.forEach((movie) => {
+    // insertar la informacion en el HTML
+    containerMovies.innerHTML += renderMovie(movie);
   });
 }
