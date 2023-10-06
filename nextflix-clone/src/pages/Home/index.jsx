d4b0591c002d2bab8cdb67778ebffe72b2bf4389
linder3hs <linderhassinger00@gmail.com>
2023-10-05 22:56:40 -0500

    ➕ feat: add titles

--- nextflix-clone/src/pages/Home/index.jsx
+++ nextflix-clone/src/pages/Home/index.jsx
@@ -1,13 +1,10 @@
-import { useState } from "react";
+import { useSelector } from "react-redux";
 
 export default function Home() {
-  const [searchText, setSearchText] = useState("");
-
-  const [titles, setTitles] = useState([]);
+  const titles = useSelector((state) => state.netflix.titles);
 
   return (
     <>
-      <h1 className="text-xl my-5">Busqueda: {searchText}</h1>
       <div className="grid grid-cols-2 gap-x-2 gap-y-3 md:gap-y-10 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
         {titles.length > 0 &&
           titles.map((title) => (
