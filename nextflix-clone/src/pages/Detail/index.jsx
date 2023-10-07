49745f8016fccf51c787ce64939fcbb40e62c8b0
linder3hs <linderhassinger00@gmail.com>
2023-10-06 22:30:23 -0500

    ➕ feat: add info

--- nextflix-clone/src/pages/Detail/index.jsx
+++ nextflix-clone/src/pages/Detail/index.jsx
@@ -17,40 +17,42 @@ export default function Detail() {
     <>
       <div className="max-w-3xl m-auto">
         <div>
-          <img
-            src="https://occ-0-3933-116.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABX6A6P7qXvE25swQSZWs1c_Do8UK0s0hs0e4GWKwJX4sdlrjhEx-N_3PE0N5E_GYquvfYyRZ5U2LNIh096EFcZo2zLvGzRG2Z6N-.webp?r=235"
-            alt=""
-          />
+          <img src={title.jawSummary.backgroundImage.url} alt="" />
         </div>
         <div className="my-3 flex gap-4 justify-between items-start">
           <div className="flex-1">
             <div className="flex gap-2">
-              <span>2020</span>
-              <span>3 temporadas</span>
-              <span className="px-1 py-[2px] border text-xs">HD</span>
+              <span>{title.jawSummary.releaseYear}</span>
+              <span>{title.jawSummary.numSeasonsLabel}</span>
+              {title.jawSummary.delivery.hasHD && (
+                <span className="px-1 py-[2px] border text-xs">HD</span>
+              )}
             </div>
             <p className="mt-2 text-sm">
-              <span className="border px-1 py-[2px] text-xs">16+</span>
-              &nbsp;&nbsp;&nbsp;<span>lenguaje inapropiado, etc</span>
+              <span className="border px-1 py-[2px] text-xs">
+                {title.jawSummary.maturity.rating.value}
+              </span>
+              &nbsp;&nbsp;&nbsp;
+              <span>
+                {title.jawSummary.maturity.rating.specificRatingReason}
+              </span>
             </p>
             <h2 className="font-semibold text-2xl my-4">T:E1 Secretos</h2>
             <p className="text-sm">
-              2019. La desaparición de un niño local aterroriza a los habitantes
-              de Winden, un pueblito alemán con una historia tan trágica como
-              extraña.
+              {title.jawSummary.currentContextualSynopsis.text}
             </p>
           </div>
           <div className="flex flex-col gap-4 w-[30%]">
             <DetailListDescription
-              title="Elenco"
+              title="Cast"
               list={title.jawSummary.cast}
             />
             <DetailListDescription
-              title="Géneros"
+              title="Genres"
               list={title.jawSummary.genres}
             />
             <DetailListDescription
-              title="Este titulo es"
+              title="This title is"
               list={title.jawSummary.tags}
             />
           </div>
