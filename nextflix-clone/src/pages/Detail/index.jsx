6f70b1c56e1ddb5bcd5cd0155172a97a2e83fe70
linder3hs <linderhassinger00@gmail.com>
2023-10-06 20:43:45 -0500

    ➕ feat: add detail styles

--- nextflix-clone/src/pages/Detail/index.jsx
+++ nextflix-clone/src/pages/Detail/index.jsx
@@ -14,7 +14,19 @@ export default function Detail() {
 
   return (
     <>
-      <h1>Detalle de Pelicula/Serie {titleId}</h1>
+      <div className="max-w-3xl m-auto">
+        <div>
+          <img
+            src="https://occ-0-3933-116.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABX6A6P7qXvE25swQSZWs1c_Do8UK0s0hs0e4GWKwJX4sdlrjhEx-N_3PE0N5E_GYquvfYyRZ5U2LNIh096EFcZo2zLvGzRG2Z6N-.webp?r=235"
+            alt=""
+          />
+        </div>
+        <div className="my-3 flex gap-2">
+          <span>2020</span>
+          <span>3 temporadas</span>
+          <span className="border px-1 py-[2px]">HD</span>
+        </div>
+      </div>
     </>
   );
 }
