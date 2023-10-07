e9d78d9fc6699c7850856e999b7cc02cfcfe3ccc
linder3hs <linderhassinger00@gmail.com>
2023-10-06 21:58:34 -0500

    ➕ feat: add component

---
+++ nextflix-clone/src/components/DetailListDescription/index.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+
+export default function DetailListDescription({ title, list }) {
+  const result = list.map((item) => item.name).join(", ");
+  
+  return (
+    <p className="text-white text-sm">
+      <span className="text-netflix-color-dark">{title}:</span> {result}
+    </p>
+  );
+}
