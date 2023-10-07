a0ec048401b658b95faf3c3169349fa0eb4d3e27
linder3hs <linderhassinger00@gmail.com>
2023-10-06 21:33:07 -0500

    ➕ feat: add view detail

--- nextflix-clone/src/pages/Detail/index.jsx
+++ nextflix-clone/src/pages/Detail/index.jsx
@@ -21,10 +21,38 @@ export default function Detail() {
             alt=""
           />
         </div>
-        <div className="my-3 flex gap-2">
-          <span>2020</span>
-          <span>3 temporadas</span>
-          <span className="border px-1 py-[2px]">HD</span>
+        <div className="my-3 flex gap-4 justify-between items-start">
+          <div className="flex-1">
+            <div className="flex gap-2">
+              <span>2020</span>
+              <span>3 temporadas</span>
+              <span className="px-1 py-[2px] border text-xs">HD</span>
+            </div>
+            <p className="mt-2 text-sm">
+              <span className="border px-1 py-[2px] text-xs">16+</span>
+              &nbsp;&nbsp;&nbsp;<span>lenguaje inapropiado, etc</span>
+            </p>
+            <h2 className="font-semibold text-2xl my-4">T:E1 Secretos</h2>
+            <p className="text-sm">
+              2019. La desaparición de un niño local aterroriza a los habitantes
+              de Winden, un pueblito alemán con una historia tan trágica como
+              extraña.
+            </p>
+          </div>
+          <div className="flex flex-col gap-4 w-[30%]">
+            <p className="text-white text-sm">
+              <span className="text-netflix-color-dark">Elenco:</span> Pepito,
+              Juanito, Oliver, más
+            </p>
+            <p className="text-white text-sm">
+              <span className="text-netflix-color-dark">Géneros:</span> Pepito,
+              Juanito, Oliver, más
+            </p>
+            <p className="text-white text-sm">
+              <span className="text-netflix-color-dark">Este titulo es:</span>{" "}
+              Surrealista, Escalofriante, Inquietante
+            </p>
+          </div>
         </div>
       </div>
     </>
