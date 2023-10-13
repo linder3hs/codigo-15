0768d46e0ee3ffa74340e5c5b21f800f357963a3
linder3hs <linderhassinger00@gmail.com>
2023-10-12 20:43:01 -0500

    ➕ feat: add dialog

--- todolist/src/components/Edit/index.jsx
+++ todolist/src/components/Edit/index.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import { PencilIcon } from "@heroicons/react/24/solid";
+import { Dialog } from "@headlessui/react";
 
 export default function Edit() {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
-      <PencilIcon className="h-6 w-6 text-blue-500" />
+      <PencilIcon
+        className="h-6 w-6 text-blue-500"
+        onClick={() => setOpen(true)}
+      />
+      <Dialog open={open} onClose={() => setOpen(false)}>
+        <Dialog.Panel>Editar informacion</Dialog.Panel>
+      </Dialog>
     </>
   );
 }
