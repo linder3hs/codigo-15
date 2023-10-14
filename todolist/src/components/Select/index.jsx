68efff83b1d618ad608732c6d86121d37932cb9a
linder3hs <linderhassinger00@gmail.com>
2023-10-13 19:54:12 -0500

    ➕ feat: add select

--- todolist/src/components/Select/index.jsx
+++ todolist/src/components/Select/index.jsx
@@ -5,35 +5,37 @@ import { ChevronUpDownIcon } from "@heroicons/react/20/solid";
 
 export default function Select({ value, onChange, items }) {
   return (
-    <>
+    <div className="">
       <Listbox value={value} onChange={onChange}>
-        <Listbox.Button className="w-full flex items-center justify-between border px-3 shadow-md py-3 rounded text-left">
-          <span>{value}</span>
-          <span>
-            <ChevronUpDownIcon className="h-5 w-5" />
-          </span>
-        </Listbox.Button>
-        <Transition
-          as={Fragment}
-          leave="transition ease-in duration-500"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <Listbox.Options className="w-full border mt-1 rounded">
-            {items.map((item) => (
-              <Listbox.Option
-                key={item}
-                value={item}
-                className={`py-2 px-3 hover:bg-green-200 hover:text-green-800 cursor-pointer ${
-                  item === value ? "bg-green-200 text-green-800" : "bg-white"
-                }`}
-              >
-                {item}
-              </Listbox.Option>
-            ))}
-          </Listbox.Options>
-        </Transition>
+        <div className="relative">
+          <Listbox.Button className="w-full flex items-center justify-between border px-3 shadow-md py-3 rounded text-left">
+            <span>{value}</span>
+            <span>
+              <ChevronUpDownIcon className="h-5 w-5" />
+            </span>
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-500"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="border mt-1 rounded absolute z-10 w-full">
+              {items.map((item) => (
+                <Listbox.Option
+                  key={item}
+                  value={item}
+                  className={`py-2 px-3 hover:bg-green-200 hover:text-green-800 cursor-pointer ${
+                    item === value ? "bg-green-200 text-green-800" : "bg-white"
+                  }`}
+                >
+                  {item}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
       </Listbox>
-    </>
+    </div>
   );
 }
