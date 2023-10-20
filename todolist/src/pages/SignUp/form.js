bf518e02d0c7aa28e51c426d7dbef03e231448a1
linder3hs <linderhassinger00@gmail.com>
2023-10-19 19:51:42 -0500

    ➕ feat: add inputs array

---
+++ todolist/src/pages/SignUp/form.js
@@ -0,0 +1,20 @@
+export const inputs = [
+  {
+    placeholder: "Ingrese su nombre",
+    name: "name",
+  },
+  {
+    placeholder: "Ingrese su apellido",
+    name: "lastname",
+  },
+  {
+    placeholder: "Ingrese su email",
+    name: "email",
+    type: "email",
+  },
+  {
+    placeholder: "Ingrese su password",
+    name: "password",
+    type: "password",
+  },
+];
--- todolist/src/pages/SignUp/index.jsx
+++ todolist/src/pages/SignUp/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Button, Card, TextField } from "../../components";
+import { inputs } from "./form";
 
 export default function SignUp() {
   const [values, setValues] = useState({
@@ -46,44 +47,20 @@ export default function SignUp() {
       <Card>
         <h1 className="text-2xl font-semibold my-5">Crear Cuenta</h1>
         <form onSubmit={handleFormSubmit} className="mb-5 flex flex-col gap-5">
-          <div>
-            <TextField
-              placeholder="Ingrese su nombre"
-              value={values.name}
-              name="name"
-              onChange={handleInputChange}
-            />
-            <span className="text-red-500 mt-1 text-sm">{errors.name}</span>
-          </div>
-          <div>
-            <TextField
-              placeholder="Ingrese su apellido"
-              value={values.lastname}
-              name="lastname"
-              onChange={handleInputChange}
-            />
-            <span className="text-red-500 mt-1 text-sm">{errors.lastname}</span>
-          </div>
-          <div>
-            <TextField
-              placeholder="Ingrese su email"
-              type="email"
-              value={values.email}
-              name="email"
-              onChange={handleInputChange}
-            />
-            <span className="text-red-500 mt-1 text-sm">{errors.email}</span>
-          </div>
-          <div>
-            <TextField
-              placeholder="Ingrese su password"
-              type="password"
-              value={values.password}
-              name="password"
-              onChange={handleInputChange}
-            />
-            <span className="text-red-500 mt-1 text-sm">{errors.password}</span>
-          </div>
+          {inputs.map((input) => (
+            <div key={input.name}>
+              <TextField
+                placeholder={input.placeholder}
+                value={values[input.name]}
+                name={input.name}
+                onChange={handleInputChange}
+                type={input.type ?? "text"}
+              />
+              <span className="text-red-500 mt-1 text-sm">
+                {errors[input.name]}
+              </span>
+            </div>
+          ))}
           <Button
             text="Crear cuenta"
             className="rounded-l w-full"
