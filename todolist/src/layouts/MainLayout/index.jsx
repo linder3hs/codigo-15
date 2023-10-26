2f6e4fea175260ba93eebc5893bd8577cdc468ef
linder3hs <linderhassinger00@gmail.com>
2023-10-25 22:48:41 -0500

    ➕ feat: add navbar

--- todolist/src/layouts/MainLayout/index.jsx
+++ todolist/src/layouts/MainLayout/index.jsx
@@ -9,6 +9,15 @@ export default function MainLayout() {
 
   return (
     <>
+      <nav className="bg-blue-400 flex justify-end items-center gap-5 pe-5 py-3">
+        <img src={user.avatar} alt="" width={40} className="rounded-full" />
+        <div className="text-sm text-white">
+          <p>
+            {user.name} {user.lastname}
+          </p>
+          <p>{user.email}</p>
+        </div>
+      </nav>
       <Outlet />
     </>
   );
--- todolist/src/pages/Home/index.jsx
+++ todolist/src/pages/Home/index.jsx
@@ -15,6 +15,7 @@ export default function Home() {
 
   useEffect(() => {
     getTasks();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
