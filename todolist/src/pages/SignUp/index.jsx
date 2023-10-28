bf5e0c5c2018ccc3ab428186c21ccaec18fc2e73
linder3hs <linderhassinger00@gmail.com>
2023-10-27 22:39:22 -0500

    test change

--- todolist/src/pages/SignUp/index.jsx
+++ todolist/src/pages/SignUp/index.jsx
@@ -30,6 +30,7 @@ export default function SignUp() {
     <div className="h-screen flex items-center justify-center max-w-md mx-auto">
       <Card>
         <h1 className="text-2xl font-semibold my-5">Crear Cuenta</h1>
+        <p>Bienvenido a todolist project</p>
         <Form
           inputs={inputs}
           errors={errors}
