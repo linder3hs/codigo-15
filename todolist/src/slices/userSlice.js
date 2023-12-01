ee4aca9970d69322afa4764a21455abc44488473
linder3hs <linderhassinger00@gmail.com>
2023-11-30 22:42:42 -0500

    login and home

--- todolist/src/slices/userSlice.js
+++ todolist/src/slices/userSlice.js
@@ -4,11 +4,14 @@ export const userSlice = createSlice({
   name: "user",
   initialState: {
     data: JSON.parse(localStorage.getItem("user")),
+    access_token: localStorage.getItem("access_token"),
   },
   reducers: {
     saveUser: (state, action) => {
-      state.data = action.payload;
-      localStorage.setItem("user", JSON.stringify(action.payload));
+      state.data = action.payload.user;
+      state.access_token = action.payload.access_token;
+      localStorage.setItem("user", JSON.stringify(action.payload.user));
+      localStorage.setItem("access_token", action.payload.access_token)
     },
   },
 });
